0f3aa7ec1947872e84e83450c3daf6334f5eb98e
Sungho <hurgj123kr@gmail.com>
2024-06-17 19:54:38 +0900

    remove Fav update

--- dist/controllers/favController.js
+++ dist/controllers/favController.js
@@ -47,7 +47,7 @@ const addFav = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 });
 exports.addFav = addFav;
 const removeFav = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { pokemonName } = req.body;
+    const { pokemonName } = req.params;
     try {
         const user = yield User_1.default.findById(req.user.id);
         if (!user) {
