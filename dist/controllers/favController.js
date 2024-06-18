72175efbbdab50e435a0358f11362900a8f4194b
Sungho <hurgj123kr@gmail.com>
2024-06-18 21:26:18 +0900

    fav error 임시조치2

--- dist/controllers/favController.js
+++ dist/controllers/favController.js
@@ -17,11 +17,12 @@ const User_1 = __importDefault(require("../models/User"));
 const getFav = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const user = yield User_1.default.findById(req.user.id);
-        console.log(user);
         if (!user) {
             return res.status(400).json({ message: "User not found" });
         }
         console.log(user.fav);
+        // 캐시 헤더 설정.
+        res.setHeader("Cache-Control", "no-store"); // 클라이언트에게 캐시하지 말라고 지시
         res.status(200).json({ fav: user.fav });
     }
     catch (err) {
