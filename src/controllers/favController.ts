5dd0c8c8b51530b90954ad5980772d99a77b88fb
Sungho <hurgj123kr@gmail.com>
2024-06-18 21:20:07 +0900

    fav error 임시조치

--- src/controllers/favController.ts
+++ src/controllers/favController.ts
@@ -5,10 +5,11 @@ import { AuthRequest } from "../middleware/authMiddleware";
 export const getFav = async (req: AuthRequest, res: Response) => {
   try {
     const user = await User.findById(req.user.id);
+    console.log(user);
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
-
+    console.log(user.fav);
     res.status(200).json({ fav: user.fav });
   } catch (err: any) {
     res.status(500).json({ message: err.message });
